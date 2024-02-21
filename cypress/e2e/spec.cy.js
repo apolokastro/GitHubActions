0c28c8c12edae56b889ec19fb90e5bb5b7fbede7
apolokastro <angelbaezc117@gmail.com>
2024-02-21 17:20:47 -0600

    1st comit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('Test Case suite', () => {
+  it('Test case to pass', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Kitchen Sink')
+  })
+
+  it('Test case to fail', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Kitchen Sinkxxx')
+  })
+})
